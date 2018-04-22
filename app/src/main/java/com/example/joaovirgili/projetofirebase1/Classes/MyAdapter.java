ba9a89faa8e695d938cb386deff460b41270c5ba
JP <sr.joaovirgili@gmail.com>
2018-04-22 18:11:00 -0300

    ajustado o card de usuario na lista de usuarios

--- app/src/main/java/com/example/joaovirgili/projetofirebase1/Classes/MyAdapter.java
+++ app/src/main/java/com/example/joaovirgili/projetofirebase1/Classes/MyAdapter.java
@@ -1,11 +1,15 @@
 package com.example.joaovirgili.projetofirebase1.Classes;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.joaovirgili.projetofirebase1.R;
@@ -26,7 +30,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     @Override
     public MyAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.user_item, parent, false);
+                .inflate(R.layout.content_user_item, parent, false);
         return new ViewHolder(v);
     }
 
@@ -35,6 +39,10 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
         User user = usersList.get(position);
         holder.textViewFirstName.setText(user.getFirstName());
         holder.textViewEmail.setText(user.getEmail());
+
+        byte[] decodedString = Base64.decode(user.getProfileImage(), Base64.DEFAULT);
+        Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+        holder.imageViewUserProfile.setImageBitmap(decodedByte);
     }
 
     @Override
@@ -46,12 +54,14 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
         public TextView textViewFirstName;
         public TextView textViewEmail;
+        public ImageView imageViewUserProfile;
 
         public ViewHolder(View itemView) {
             super(itemView);
 
             textViewEmail = itemView.findViewById(R.id.textViewEmail);
             textViewFirstName = itemView.findViewById(R.id.textViewFirstName);
+            imageViewUserProfile = itemView.findViewById(R.id.cardUserProfile);
 
         }
     }
